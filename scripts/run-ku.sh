44171ee4b30d27364526afb90c82bc5957d7e14e
shermp <14854761+shermp@users.noreply.github.com>
2019-03-17 13:33:03 +1300

    Add some more verbose output

--- scripts/run-ku.sh
+++ scripts/run-ku.sh
@@ -10,53 +10,71 @@
 KU_DIR="$1"
 KU_TMP_DIR="$2"
 . ./nickel-usbms.sh
+# Set terminal color escape sequences
+END="\033[0m"
+RED="\033[31;1m"
+YELLOW="\033[33;1m"
+GREEN="\033[32;1m"
 
-./fbink -y 0 -Y 100 -m -p -r "Entering USBMS mode..."
+./fbink -y 0 -Y 100 -m -p -r -q "Entering USBMS mode..."
+printf "${GREEN}Inserting USB${END}\n"
 insert_usb
 
+printf "${GREEN}Scanning for Button${END}\n"
 BS_TIMEOUT=0
 while ! ./button_scan -p
 do
     # If the button scan hasn't succeeded in 5 seconds, assume it's not going to...
     if [ $BS_TIMEOUT -ge 20 ]; then
-        return 0
+        exit 1
     fi
     # Button scan hasn't succeeded yet. wait a bit (250ms), then try again
     usleep 250000
     BS_TIMEOUT=$(( BS_TIMEOUT + 1 ))
 done
-
+printf "${GREEN}(Re)mounting onboard${END}\n"
 if ! mount_onboard; then
+    printf "${RED}Onboard did not remount${END}\n"
     remove_usb
+    exit 1
+
 fi
+printf "${GREEN}Enabling WiFi${END}\n"
 if ! enable_wifi; then
+    printf "${RED}WiFi did not enable. Aborting!${END}\n"
     unmount_onboard
     remove_usb
+    exit 1
 fi
 
-./fbink -y 0 -Y 100 -m -p -r "USBMS mode entered..."
+./fbink -y 0 -Y 100 -m -p -r -q "USBMS mode entered..."
 
+printf "${GREEN}Running Kobo-UNCaGED${END}\n"
 KU_BIN="${MNT_ONBOARD_NEW}/${KU_DIR}/bin/kobo-uncaged"
 $KU_BIN "-onboardmount=${MNT_ONBOARD_NEW}"
 KU_RES=$?
+printf "${GREEN}Leaving USBMS${END}\n"
 ./fbink -y 0 -Y 100 -m -p -r "Leaving USBMS..."
+printf "${GREEN}Disabling WiFi${END}\n"
 disable_wifi
 ./fbink -y 0 -Y 100 -m -p -r "Wifi Disabled..."
+printf "${GREEN}Unmounting onboard${END}\n"
 unmount_onboard
 ./fbink -y 0 -Y 100 -m -p -r "Onboard Unmounted..."
 
-./button_scan -w -u
+./button_scan -w -u -q
 BS_RES=$?
 if [ $KU_RES -eq 1 ] && $BS_RES; then
-    ./fbink -y 0 -Y 100 -m -p -r "Entering USBMS mode..."
+    printf "${GREEN}Updating metadata${END}\n"
+    ./fbink -y 0 -Y 100 -m -p -r -q "Entering USBMS mode..."
     insert_usb
 
     BS_TIMEOUT=0
-    while ! ./button_scan -p
+    while ! ./button_scan -p -q
     do
         # If the button scan hasn't succeeded in 5 seconds, assume it's not going to...
         if [ $BS_TIMEOUT -ge 20 ]; then
-            return 0
+            exit 1
         fi
         # Button scan hasn't succeeded yet. wait a bit (250ms), then try again
         usleep 250000
@@ -64,10 +82,11 @@ if [ $KU_RES -eq 1 ] && $BS_RES; then
     done
     if ! mount_onboard; then
         remove_usb
+        exit 1
     fi
     $KU_BIN "-onboardmount=${MNT_ONBOARD_NEW} -metadata"
     unmount_onboard
-    ./fbink -y 0 -Y 100 -m -p -r "Onboard Unmounted..."
+    ./fbink -y 0 -Y 100 -m -p -r -q "Onboard Unmounted..."
     remove_usb
 fi
     
