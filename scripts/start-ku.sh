319b38ea457d546666f3a059a75c47efd21dee1b
NiLuJe <ninuje@gmail.com>
2019-05-20 02:42:22 +0200

    Cleanup /tmp when we're done

--- scripts/start-ku.sh
+++ scripts/start-ku.sh
@@ -5,16 +5,21 @@ KU_TMP_DIR=/tmp/ku
 
 # Copy essential programs and scripts to /tmp, to ensure they will be available
 # during USBMS session
+KU_REQ_FILES="scripts/run-ku.sh scripts/nickel-usbms.sh bin/button_scan bin/fbink"
 mkdir -p "$KU_TMP_DIR"
-cp "/mnt/onboard/${KU_DIR}/scripts/run-ku.sh" "$KU_TMP_DIR"
-cp "/mnt/onboard/${KU_DIR}/scripts/nickel-usbms.sh" "$KU_TMP_DIR"
-cp "/mnt/onboard/${KU_DIR}/bin/button_scan" "$KU_TMP_DIR"
-cp "/mnt/onboard/${KU_DIR}/bin/fbink" "$KU_TMP_DIR"
+for cur_file in ${KU_REQ_FILES}; do
+    cp -f "/mnt/onboard/${KU_DIR}/${cur_file}" "${KU_TMP_DIR}/${cur_file##*/}"
+done
 
 # Check if we have a ku.toml file. If not, copy it from the default file
 if [ ! -f "/mnt/onboard/${KU_DIR}/config/ku.toml" ]; then
-    cp "/mnt/onboard/${KU_DIR}/config/ku.toml.default" "/mnt/onboard/${KU_DIR}/config/ku.toml"
+    cp -f "/mnt/onboard/${KU_DIR}/config/ku.toml.default" "/mnt/onboard/${KU_DIR}/config/ku.toml"
 fi
 
 cd "$KU_TMP_DIR" || exit 255
-exec ./run-ku.sh "$KU_DIR" "$KU_TMP_DIR"
+./run-ku.sh "$KU_DIR" "$KU_TMP_DIR"
+
+# Cleanup behind us
+for cur_file in ${KU_REQ_FILES}; do
+    rm -f "${KU_TMP_DIR}/${cur_file##*/}"
+done
