6b1c7bb2f9d9e1ebfaeba0f84100642492512d34
shermp <14854761+shermp@users.noreply.github.com>
2020-02-11 23:54:15 +1300

    Bump CLI fbink to v1.21.0

--- build-ku.sh
+++ build-ku.sh
@@ -58,7 +58,7 @@ cd ./Build/prerequisites || exit 1
 if [ ! -f ./output/fbink ] && [ ! -f ./output/button_scan ]; then
     printf "%bFBInk binaries not found. Building from source%b\n" "${YELLOW}" "${END}"
     if [ ! -d ./FBInk ]; then
-        git clone --recursive --branch v1.20.2 https://github.com/NiLuJe/FBInk.git
+        git clone --recursive --branch v1.21.0 https://github.com/NiLuJe/FBInk.git
     fi
     cd ./FBInk || exit 1
     make clean
