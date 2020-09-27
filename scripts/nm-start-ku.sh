aa2060542dddfc18fbbaee3f3c6406b94ee19609
shermp <14854761+shermp@users.noreply.github.com>
2020-09-27 17:50:54 +1300

    Reduce non-error toasts to 1 second

--- scripts/nm-start-ku.sh
+++ scripts/nm-start-ku.sh
@@ -37,30 +37,30 @@ logmsg "I" "Enabled loopback interface"
 
 cd ${KU_DIR}
 
-logmsg "I" "Starting Kobo UNCaGED" 3000
+logmsg "I" "Starting Kobo UNCaGED" 1000
 $KU_BIN
 KU_RES=$?
 if [ "$KU_RES" -eq 0 ] ; then
     if [ -f $KU_REPL_MD ] ; then
-        logmsg "I" "Updating replacement book filesize(s)" 3000
+        logmsg "I" "Updating replacement book filesize(s)" 1000
         call_sqlite "$KU_REPL_MD"
     fi
     # Always run library rescan, just in case. Especially to catch book deletion
-    logmsg "I" "Running library rescan" 3000
+    logmsg "I" "Running library rescan" 1000
     qndb -s pfmDoneProcessing -m pfmRescanBooksFull
     if [ -f $KU_REPL_MD ] || [ -f $KU_UPDATE_MD ] ; then
         if [ -f $KU_UPDATE_MD ] ; then
-            logmsg "I" "Updating metadata" 3000
+            logmsg "I" "Updating metadata" 1000
             call_sqlite "$KU_UPDATE_MD"
-            logmsg "I" "Running library rescan after metadata update" 3000
+            logmsg "I" "Running library rescan after metadata update" 1000
             qndb -s pfmDoneProcessing -m pfmRescanBooksFull
         fi
-        logmsg "I" "Metadata updated" 3000
+        logmsg "I" "Metadata updated" 1000
     fi
     [ -f $KU_REPL_MD ] && rm $KU_REPL_MD
     [ -f $KU_UPDATE_MD ] && rm $KU_UPDATE_MD
 elif [ "$KU_RES" -eq 250 ] ; then
-    logmsg "I" "Running precautionary library rescan" 3000
+    logmsg "I" "Running precautionary library rescan" 1000
     qndb -s pfmDoneProcessing -m pfmRescanBooksFull
 fi
 
@@ -70,4 +70,4 @@ cd -
 ip link set lo down
 logmsg "I" "Disabled loopback interface"
 
-logmsg "I" "Kobo UNCaGED exiting" 3000
+logmsg "I" "Kobo UNCaGED exiting" 1000
